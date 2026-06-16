a231cd869161ac99e78d2620e9de034bb092a53e
kbchip <charlieshaskins@gmail.com>
2026-06-16 16:57:51 -0700

    Create server.js with barebones routes

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+let postArray = [];
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        posts: postArray
+    });
+});
+
+app.post("/create", (req, res) => {
+    // TODO: create new post
+});
+
+app.patch("/edit", (req, res) => {
+    // TODO: edit existing post
+});
+
+app.delete("/delete", (req, res) => {
+    // TODO: delete existing post
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
